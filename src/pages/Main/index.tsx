c99057e70f0be034ad4f5b6b8b98147cb96f1dc1
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-22 13:57:55 -0300

    Created class .title and changed .calculator class

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,7 +1,33 @@
+import Button from '../../components/Button'
+
 import styles from './styles.module.css'
 
 const Main = () => {
-  return <div className={styles.calculator}></div>
+  return (
+    <>
+      <h1 className={styles.title}>Calculator</h1>
+
+      <div className={styles.calculator}>
+        <Button label="AC" />
+        <Button label="/" />
+        <Button label="7" />
+        <Button label="8" />
+        <Button label="9" />
+        <Button label="x" />
+        <Button label="4" />
+        <Button label="5" />
+        <Button label="6" />
+        <Button label="-" />
+        <Button label="1" />
+        <Button label="2" />
+        <Button label="3" />
+        <Button label="+" />
+        <Button label="0" />
+        <Button label="." />
+        <Button label="=" />
+      </div>
+    </>
+  )
 }
 
 export default Main
