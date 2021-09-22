2ec7f309248ec708971ae6185a7e15f912f646d0
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-22 16:52:58 -0300

    Finished Main page

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,35 +1,97 @@
+import { useState } from 'react'
+
 import Button from '../../components/Button'
 import Display from '../../components/Display'
 
 import styles from './styles.module.css'
 
 const Main = () => {
+  const [value, setValue] = useState('')
+  const [values, setValues] = useState<string[]>([])
+  const [operations, setOperations] = useState<string[]>([])
+  const [isResult, setIsResult] = useState(false)
+
+  const clear = () => {
+    setValue('')
+    setValues([])
+    setIsResult(false)
+    setOperations([])
+  }
+
+  const calc = (calcValues: (string | number)[]) => {
+    let calcString = ''
+
+    calcValues.forEach((value, index) => {
+      if (!operations[index]) calcString += value
+      else calcString += value + operations[index]
+    })
+
+    setValues([eval(calcString)])
+    setIsResult(true)
+    setOperations([])
+  }
+
+  const setOperation = (operation: unknown) => {
+    if (typeof operation !== 'string') return
+    setValues([...values, value])
+    isResult && setIsResult(false)
+
+    if (operation === 'x') setOperations([...operations, '*'])
+    else if (operation === '=') calc([...values, value])
+    else setOperations([...operations, operation])
+
+    setValue('')
+  }
+
+  const addDigit = (value: string | number) => {
+    if (isResult) {
+      setIsResult(false)
+      setValues([])
+    }
+
+    setValue(oldValue => oldValue + value)
+  }
+
   return (
-    <>
-      <h1 className={styles.title}>Calculator</h1>
+    <div className={styles.calculator}>
+      <Display
+        value={value || values[values.length - 1] || 0}
+        operation={
+          isResult
+            ? '='
+            : operations[operations.length - 1] === '*'
+            ? 'x'
+            : operations[operations.length - 1]
+        }
+      />
 
-      <div className={styles.calculator}>
-        <Display value={100} />
+      {/* Row 1 */}
+      <Button label="AC" cols={3} onClick={clear} />
+      <Button label="/" isOperation onClick={setOperation} />
+      <Button label="7" onClick={addDigit} />
+      <Button label="8" onClick={addDigit} />
 
-        <Button label="AC" />
-        <Button label="/" />
-        <Button label="7" />
-        <Button label="8" />
-        <Button label="9" />
-        <Button label="x" />
-        <Button label="4" />
-        <Button label="5" />
-        <Button label="6" />
-        <Button label="-" />
-        <Button label="1" />
-        <Button label="2" />
-        <Button label="3" />
-        <Button label="+" />
-        <Button label="0" />
-        <Button label="." />
-        <Button label="=" />
-      </div>
-    </>
+      {/* Row 2 */}
+      <Button label="9" onClick={addDigit} />
+      <Button label="x" isOperation onClick={setOperation} />
+      <Button label="4" onClick={addDigit} />
+      <Button label="5" onClick={addDigit} />
+
+      {/* Row 3 */}
+      <Button label="6" onClick={addDigit} />
+      <Button label="-" isOperation onClick={setOperation} />
+      <Button label="1" onClick={addDigit} />
+      <Button label="2" onClick={addDigit} />
+
+      {/* Row 4 */}
+      <Button label="3" onClick={addDigit} />
+      <Button label="+" isOperation onClick={setOperation} />
+      <Button label="0" cols={2} onClick={addDigit} />
+      <Button label="." onClick={addDigit} />
+
+      {/* Row 5 */}
+      <Button label="=" isOperation onClick={setOperation} />
+    </div>
   )
 }
 
