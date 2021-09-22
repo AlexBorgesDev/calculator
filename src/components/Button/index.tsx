1012cb4ba07266c98e447276d424398ede35111b
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-22 14:48:00 -0300

    Created new props for Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,24 @@ import { memo } from 'react'
 import styles from './styles.module.css'
 
 export interface ButtonProps {
-  label?: string | number
+  cols?: 2 | 3
+  label: string | number
+  isOperation?: boolean
+  onClick?: (value: string | number) => void
 }
 
 const Button = (props: ButtonProps) => {
-  return <button className={styles.container}>{props.label}</button>
+  return (
+    <button
+      className={`${styles.container} ${
+        props.isOperation ? styles.operation : ''
+      }`}
+      data-col={props.cols || 1}
+      onClick={() => props.onClick && props.onClick(props.label)}
+    >
+      {props.label}
+    </button>
+  )
 }
 
 export default memo(Button)
