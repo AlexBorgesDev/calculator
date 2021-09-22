92447194fa1437bb56dc3a5b9db3e43e50ea4c9b
Alex Borges Ramos <alexborgesramos2002@gmail.com>
2021-09-22 14:53:24 -0300

    Changed Display component

--- src/components/Display/index.tsx
+++ src/components/Display/index.tsx
@@ -4,10 +4,15 @@ import styles from './styles.module.css'
 
 export interface DisplayProps {
   value: string | number
+  operation?: string
 }
 
-const Display = ({ value }: DisplayProps) => {
-  return <div className={styles.container}>{value}</div>
+const Display = ({ value, operation }: DisplayProps) => {
+  return (
+    <div className={styles.container}>
+      <span>{operation}</span> {value}
+    </div>
+  )
 }
 
 export default memo(Display)
